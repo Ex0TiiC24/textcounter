83138e96edd0b47494ebdde194467ed2f3be4dab
Ex0TiiC24 <taiyophamadadar6@gmail.com>
2024-10-23 20:31:51 +0700

    cool

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
 function App() {
+
+
+  const [count,setCount] = useState("slolwodlawo")
+
+  function counter(valuew){
+    const temp = valuew.split('')
+    const counti = temp.length
+    setCount(counti)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="w-full h-screen bg-zinc-800 text-white flex flex-col col-span-1 justify-center items-center">
+      <div class="text-black flex flex-col justify-center items-center bg-white w-fit h-96 rounded-md hover:bg-slate-200 transition-all ">
+        SimpleCounter
+        <div class="text-black text-center my-3 font-semibold text-xl">
+          {count}
+        </div>
+        
+        <div class="mx-4">
+          <input class="border-2 w-96 text-black" onChange={e => counter(e.target.value)}></input>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
